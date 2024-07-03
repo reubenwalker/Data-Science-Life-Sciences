a5d779279613dc5602f4ff29c0f78c85b36ded98
reubenwalker <reuben.scott.walker@gmail.com>
2024-07-03 15:13:18 +0200

    Subscripted dataframe

--- project.py
+++ project.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 syn = synapseclient.Synapse()
 syn.login(authToken="dummy")
-
 entity = syn.get("syn55234671")
 entity2 = syn.get("syn55234672")
 entity3 = syn.get("syn55234673")
@@ -44,6 +43,7 @@ df_patient_clinical = pd.merge(df_patient[['PATIENT_ID', 'SEX']], df_clinical[['
 
 df_clinical_merge = df_patient_clinical[['SAMPLE_ID', 'SEX', 'AGE_AT_SEQ_REPORT', 'CANCER_TYPE']].set_index('SAMPLE_ID')
 
+df_CNA_T = df_CNA_T.add_suffix('_CNA')
 
 df_CNA_clinical = pd.merge(df_CNA_T, df_clinical_merge, left_index=True, right_index=True)
 df_CNA_clinical = df_CNA_clinical.dropna(axis=1) #Drop empty columns
@@ -53,7 +53,8 @@ del df_patient
 del df_clinical
 del df_patient_clinical
 del df_clinical_merge
-df_CNA_clinical.to_csv('mergedClinicalCNA.csv')
+# df_CNA_clinical.to_csv('mergedClinicalCNA.csv')
+
 
 df_sv = pd.read_csv(entity5.path, delimiter="\t")#, nrows=2)
 #df_mutations = pd.read_csv(entity4.path, delimiter="\t", nrows=1000)
@@ -77,6 +78,8 @@ df_sv = df_sv.loc[somatic_mask, ]
 df_svCounts = df_sv.groupby(['Sample_Id','Site1_Hugo_Symbol'])['Class'].count().unstack() 
 df_svCounts = df_svCounts.fillna(0) #Fill missing counts with 0
 
+df_svCounts = df_svCounts.add_suffix('_SV')
+
 df_CNA_SV = pd.merge(df_svCounts, df_CNA_clinical,left_index=True, right_index=True) #on=['INSTITUTION','SAMPLE'])
 
 del df_CNA_clinical
@@ -84,14 +87,14 @@ del df_sv
 del df_svCounts
 
 #Let's also remove age <18, >89
+import pandas as pd
 # df = pd.read_csv('CNA_SV.csv').set_index('Unnamed: 0')
 # df_CNA_SV = df
-ageMask = (df_CNA_SV['AGE_AT_SEQ_REPORT'] == '<18') | (df_CNA_SV['AGE_AT_SEQ_REPORT'] == '>89')
+ageMask = ((df_CNA_SV['AGE_AT_SEQ_REPORT'] == '<18') | (df_CNA_SV['AGE_AT_SEQ_REPORT'] == '>89') | (df_CNA_SV['AGE_AT_SEQ_REPORT'] == 'Unknown'))
 df_CNA_SV = df_CNA_SV[~ageMask]
 sexMask = df_CNA_SV['SEX'] != 'Unknown'
 df_CNA_SV = df_CNA_SV[sexMask]
 df_CNA_SV.to_csv('CNA_SV.csv')
-
 
 
 #From the mutation data, we want
